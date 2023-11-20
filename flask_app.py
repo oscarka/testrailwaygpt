4e7e1a47a1157f7bff40d857cf6b49bd1abe8e1d
oscarka <ka-0724@hotmail.com>
2023-11-20 16:45:16 +0800

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,30 @@
+
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+# Azure OpenAI API details
+azure_openai_endpoint = 'https://oscarchatapi.openai.azure.com/deployments/oscargpt4-32/completions?api-version=2023-06-13'
+api_key = 'YOUR_API_KEY'  # Replace with your actual API key
+default_prompt = '你可否同时扮演一个医学专家和保险精算师...'
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    customer_data = request.json.get('customerData')
+    prompt = default_prompt + '\n\n' + customer_data
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_key}'
+    }
+    data = {
+        'prompt': prompt,
+        'max_tokens': 150
+    }
+
+    response = requests.post(azure_openai_endpoint, json=data, headers=headers)
+    return jsonify(response.json())
+
+if __name__ == '__main__':
+    app.run(debug=True)
