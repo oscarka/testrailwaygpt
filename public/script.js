c3d6c38b78484906177260b5b0a15c133f639b38
oscarka <ka-0724@hotmail.com>
2023-11-22 11:07:30 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,32 +2,29 @@ document.getElementById('submitButton').addEventListener('click', function() {
     var customerData = document.getElementById('customerInput').value;
     var responseContainer = document.getElementById('response');
     var spinner = document.querySelector('.loading-spinner');
-    spinner.style.display = 'inline-block'; // Show spinner
+    spinner.style.display = 'inline-block'; // 显示加载指示器
 
     fetch('/analyze', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache' // 确保不使用缓存
         },
         body: JSON.stringify({ customerData: customerData })
     })
     .then(response => response.json())
     .then(data => {
-        console.log(data); // 打印接收到的完整数据
         if (data.choices && data.choices.length > 0 && data.choices[0].message && data.choices[0].message.content) {
-        var content = data.choices[0].message.content;
-        document.getElementById('response').textContent = content;
+            // 确保只显示 content 字段的内容
+            responseContainer.textContent = data.choices[0].message.content;
         } else {
-        document.getElementById('response').textContent = 'Received unexpected data structure from the API.';
-        console.log('Unexpected data:', data);
+            // 如果预期的 content 字段不存在，显示错误消息
+            responseContainer.textContent = 'API返回的数据结构不符合预期。';
         }
+        spinner.style.display = 'none'; // 隐藏加载指示器
     })
     .catch(error => {
-        // Handle any errors that occurred during fetch or data processing
-        responseContainer.textContent = 'Error: ' + error.message;
-    })
-    .finally(() => {
-        // Always hide the spinner after processing is complete
-        spinner.style.display = 'none';
+        responseContainer.textContent = '发生错误：' + error.message;
+        spinner.style.display = 'none'; // 隐藏加载指示器
     });
 });
