276aff490022b5fd93ee1866e03c42c3404226b1
oscarka <ka-0724@hotmail.com>
2023-11-22 10:41:21 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 document.getElementById('submitButton').addEventListener('click', function() {
     var customerData = document.getElementById('customerInput').value;
-    var button = this;
+    var responseContainer = document.getElementById('response');
     var spinner = document.querySelector('.loading-spinner');
     spinner.style.display = 'inline-block'; // Show spinner
 
@@ -11,16 +11,28 @@ document.getElementById('submitButton').addEventListener('click', function() {
         },
         body: JSON.stringify({ customerData: customerData })
     })
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok.');
+        }
+        return response.json();
+    })
     .then(data => {
-        // Assuming the data structure includes data.choices[0].message.content
-        var content = data.choices[0].message.content;
-        document.getElementById('response').textContent = content; // Display content
-        spinner.style.display = 'none'; // Hide spinner
+        // Check if the data structure has the expected content field
+        if (data.choices && data.choices.length > 0 && data.choices[0].message && data.choices[0].message.content) {
+            var content = data.choices[0].message.content;
+            responseContainer.textContent = content; // Display content
+        } else {
+            // If the expected content field is not present, show a default message or the whole response for debugging
+            responseContainer.textContent = 'Received unexpected data structure from the API.';
+            console.log(data); // Log the whole data for debugging
+        }
     })
     .catch(error => {
         console.error('Error:', error);
-        document.getElementById('response').textContent = 'Error: ' + error;
-        spinner.style.display = 'none'; // Hide spinner
+        responseContainer.textContent = 'Error: ' + error.message;
+    })
+    .finally(() => {
+        spinner.style.display = 'none'; // Hide spinner in both cases (success/error)
     });
 });
