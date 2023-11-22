c83e37f21439c3976d2e446e45b594a1d3b5d8bb
oscarka <ka-0724@hotmail.com>
2023-11-22 10:30:53 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,8 @@
 
 document.getElementById('submitButton').addEventListener('click', function() {
+    // Display the loading indicator
+    document.getElementById('loading').style.display = 'block';
+
     var customerData = document.getElementById('customerInput').value;
     fetch('/analyze', {
         method: 'POST',
@@ -10,9 +13,16 @@ document.getElementById('submitButton').addEventListener('click', function() {
     })
     .then(response => response.json())
     .then(data => {
+        // Hide the loading indicator
+        document.getElementById('loading').style.display = 'none';
+        // Display the data
         document.getElementById('response').innerHTML = JSON.stringify(data, null, 2);
+        // Scroll to the top of the response area
+        document.getElementById('response').scrollTop = 0;
     })
     .catch(error => {
+        // Hide the loading indicator
+        document.getElementById('loading').style.display = 'none';
         console.error('Error:', error);
         document.getElementById('response').innerHTML = 'Error: ' + error;
     });
