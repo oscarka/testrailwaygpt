be7346e3059158ab797ead81524b77f1aefcea26
oscarka <ka-0724@hotmail.com>
2023-11-24 14:14:18 +0800

    Add files via upload

---
+++ public/static/script.js
@@ -0,0 +1,19 @@
+
+document.getElementById('submitButton').addEventListener('click', function() {
+    var customerData = document.getElementById('customerInput').value;
+    fetch('/analyze', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ customerData: customerData })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('response').innerHTML = JSON.stringify(data, null, 2);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('response').innerHTML = 'Error: ' + error;
+    });
+});
